b0eb40e9f35b756f42ef36c2512ef76d7e5ce5d9
Saranya-Satheesh1109 <satheeshsaranya98@gmail.com>
2026-05-16 12:52:05 +0530

    Add files via upload
    week1/day2

---
+++ Day2/oddOrEven.js
@@ -0,0 +1,10 @@
+let variable = 11
+function isOddOrEven(variable) {
+    if(variable%2 == 0) {
+        console.log("Even")
+    }else {
+        console.log("Odd")
+    }
+
+}
+isOddOrEven(variable)
