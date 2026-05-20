20f1dd8a2e4a8aed8433544e18cb5cbaafd56337
Saranya-Satheesh1109 <satheeshsaranya98@gmail.com>
2026-05-20 16:36:08 +0530

    Update StringSort.js

--- Day4/StringSort.js
+++ Day4/StringSort.js
@@ -1,6 +1,6 @@
 //Declaring variables
-let anagram1 = "Hello"
-let anagram2 = "World"
+let anagram1 = "He llo"
+let anagram2 = "Wo rld"
 
 //Calling function by passing the arguments
 isAnagram(anagram1,anagram2)
@@ -9,8 +9,8 @@ isAnagram(anagram1,anagram2)
 function isAnagram(val1, val2) {
     
     //Using methods to get the sorted strings
-    let ab = anagram1.trim().toLowerCase().split("").sort().toString()
-    let bc = anagram2.trim().toLowerCase().split("").sort().toString()
+    let ab = anagram1.replace(/\s/g, '').toLowerCase().split("").sort().toString()
+    let bc = anagram2.replace(/\s/g, '').toLowerCase().split("").sort().toString()
     //Condition to check the Anagram logic
     if(ab === bc) {
         console.log("True")
