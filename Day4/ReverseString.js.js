5db911d35fa7d32709b732c1921529169e0d9863
Saranya-Satheesh1109 <satheeshsaranya98@gmail.com>
2026-05-20 13:03:54 +0530

    Update ReverseString.js.js

--- Day4/ReverseString.js.js
+++ Day4/ReverseString.js.js
@@ -1,7 +1,7 @@
 let empName = "Saranya"
 let v = empName.split("")//Splitting the variable
 
-//Using for loop to reverse the String
+//Using for loop to reverse the string
 for(let i = v.length-1 ; i >= 0 ; i--) {
    console.log(v[i])
 }
