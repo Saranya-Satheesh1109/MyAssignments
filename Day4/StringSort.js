4a0ebf5a79ab52d2c7181ec3486ca043284a9e52
Saranya-Satheesh1109 <satheeshsaranya98@gmail.com>
2026-05-21 12:56:47 +0530

    Update StringSort.js

--- Day4/StringSort.js
+++ Day4/StringSort.js
@@ -9,10 +9,10 @@ isAnagram(anagram1,anagram2)
 function isAnagram(val1, val2) {
     
     //Using methods to get the sorted strings
-    let ab = anagram1.replace(/\s/g, '').toLowerCase().split("").sort().toString()
-    let bc = anagram2.replace(/\s/g, '').toLowerCase().split("").sort().toString()
+    let updatedString1 = val1.replace(/\s/g, '').toLowerCase().split("").sort().toString()
+    let updatedString2 = val2.replace(/\s/g, '').toLowerCase().split("").sort().toString()
     //Condition to check the Anagram logic
-    if(ab === bc) {
+    if(updatedString1 === updatedString2) {
         console.log("True")
     } else {
         console.log("False")
