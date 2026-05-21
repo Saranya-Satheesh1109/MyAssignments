4881f9a90aa4cd73cf6e032763f5c2163f88c9bf
Saranya-Satheesh1109 <satheeshsaranya98@gmail.com>
2026-05-21 06:05:27 +0530

    week2/day5

---
+++ Day5/ArrayMethodImplementation.js
@@ -0,0 +1,27 @@
+let array = [3,4,'d',"Java",null,'e','Playwright','6']//Declaring array variable
+array.push("Number")//Using push() method to add an element at the last index value
+console.log(array)
+
+array.pop()//Using pop() method to remove an element from the last index value
+console.log(array)
+
+array.shift()//Using shift() method to remove an element from the first index value
+console.log(array)
+
+array.unshift('a')//Using unsf=hift() method to add an element at the first index value
+console.log(array)
+
+console.log(array.includes(null))//Using includes() method to return true or false based on the array containing the given value
+
+console.log(array.indexOf(4))//Using indexOf() method to return the index of the given value
+
+console.log(array.join(""))//Using join("") method to convert an array to string
+
+console.log(array.reverse())//Using reverse() method to reverse the elements of an array
+
+console.log(array.sort())//Using sort() method to sort the elements of an array
+
+
+
+
+
