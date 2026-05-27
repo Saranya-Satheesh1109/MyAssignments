c3e211e3fcb6fc5af739c9e007f1c0bddfb77033
Saranya-Satheesh1109 <satheeshsaranya98@gmail.com>
2026-05-27 08:36:13 +0530

    Update LeaftapsLogin.spec.ts

--- Week3Day2/LeaftapsLogin.spec.ts
+++ Week3Day2/LeaftapsLogin.spec.ts
@@ -2,16 +2,20 @@ import test from "@playwright/test"
 
 test("Leaftaps Login", async({page})=>{
 
+    //Login into Leaftaps application using locators for each field
+
     await page.goto("https://leaftaps.com/opentaps/control/main")
     await page.locator("#username").fill("democsr")
     await page.locator("[name='PASSWORD']").fill("crmsfa")
     await page.locator(".decorativeSubmit").click()
 
-    
+    //Getting the page title after clicking the submit button while using timeout to prevent error due to page loading
     console.log(await page.title())
     await page.locator(`text='CRM/SFA'`).click()
     await page.waitForTimeout(5000)
     console.log(await page.title())
+
+    //Creating a Lead using different locators for each field and printing the page title of the page after Lead creation
 
     await page.locator(`text='Leads'`).click()
     await page.waitForTimeout(5000)
