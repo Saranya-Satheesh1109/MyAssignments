e843c23410481b2afd2677ee14f838b7c9fa09e8
Saranya-Satheesh1109 <satheeshsaranya98@gmail.com>
2026-05-30 09:02:12 +0530

    Add files via upload

---
+++ Week3Day3/LeadAssignment.spec.ts
@@ -0,0 +1,26 @@
+import test from "@playwright/test"
+
+test("Create Lead Salesforce", async({page})=>{
+
+    await page.goto("https://login.salesforce.com")
+    await page.locator("#username").fill("dilipkumar.rajendran@testleaf.com")
+    await page.locator("#password").fill("TestLeaf@2025")
+    await page.locator("#Login").click()
+    await page.waitForTimeout(8000)
+    await page.locator("//div/span[contains(text(),'App Launcher')]").click()
+    await page.waitForTimeout(8000)
+    await page.locator("//button[text()='View All']").click()
+    await page.waitForTimeout(2000)
+    await page.locator("//span/p[text()='Leads']").click()
+    await page.locator("//a/div[text()='New']").click()
+    await page.locator("[name='salutation']").click()
+    await page.waitForTimeout(3000)
+    await page.locator("[name='firstName']").fill("Saranya")
+    await page.locator("[name='lastName']").fill("Satheesh")
+    await page.locator("[name='Company']").fill("Learning Academy")
+    await page.locator("[name='SaveEdit']").click()
+   
+
+
+
+})
