3be8832e611448e7d06122c4fecc3882a9a5ea94
Saranya-Satheesh1109 <satheeshsaranya98@gmail.com>
2026-06-07 08:09:15 +0530

    Update PVRAssignment.spec.ts

--- Marathon Assignment/PVRAssignment.spec.ts
+++ Marathon Assignment/PVRAssignment.spec.ts
@@ -3,6 +3,7 @@ import test,{expect} from "@playwright/test"
 test("PVR Assignment", async({page})=>{
 
     await page.goto("https://www.pvrcinemas.com/")
+    //Choosing Cinema and its related dropdowns
     await page.locator(".cities-overlay.cities-active").nth(5).click()
     await page.waitForTimeout(3000)
     await page.locator(".cinemas-inactive").click()
@@ -16,10 +17,11 @@ test("PVR Assignment", async({page})=>{
     await page.locator("//li/span[text()='PARIMALA AND CO']").click()
     await page.locator("//span/span[text()='10:20 AM']").click()
     await page.waitForTimeout(2000)
-    await page.getByLabel("Submit").click()
+    await page.getByLabel("Submit").click()//Clicking on Book option
     await page.waitForTimeout(5000)
-    await page.locator("//button[text()='Accept']").click()
-    await page.locator("(//td/span[text()='15'])[3]").click()
+    await page.locator("//button[text()='Accept']").click()//Clcking on Accept button on the pop up
+    await page.locator("(//td/span[text()='15'])[3]").click()//Selecting the seat
+    //Getting the Booking summary deatils
     await expect.soft(page.locator("//h5[text()='PARIMALA AND CO']")).toHaveText("PARIMALA AND CO")
     const movieName = await page.locator("//h5[text()='PARIMALA AND CO']").innerText()
     console.log("The movie Name is : " + movieName)
@@ -31,9 +33,4 @@ test("PVR Assignment", async({page})=>{
     console.log(title)
     expect.soft(title).toBe("PVR Cinemas")
     
-
-
-
-
 })
-//li/span[text()='10:20 AM']
